35666f26b3393417f137700ba332b1f7bcc62d8e
nakaly <nakaly@linecorp.com>
2018-09-25 03:33:34 +0900

    Can read digit

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,35 @@ fn main() {
     }
     let p = args.get(1).unwrap();
 
-    println!("{:?}", p.len());
-    let mut value :i32 = 0;
-    let mut index :usize = 0;
-    while index != p.len() - 1 {
-        let value_index = eval(p.to_string(), index);
-        value = value_index.0;
-        index = value_index.1;
+fn parse(input: &str) -> u32 {
+    let mut code = input;
+    let mut code = & mut code;
+
+    while code.len() > 0 {
+        let result = eval( &mut  code);
+        return result;
+    }
+    return 0;
+}
+
+fn eval(code:  & mut &str) -> u32 {
+    println!("{:?}", &code[0..1]);
+    let chars = code.chars();
+    let mut pos: usize = 0;
+    let mut num: u32 = 0;
+    for (i, c) in chars.enumerate() {
+        if let Some(digit) = c.to_digit(10) {
+            pos = i;
+            num = digit + num * 10;
+            println!("p:{}", i);
+            println!("n:{}", num);
+            
+        }
+        if c.is_whitespace() {
+            println!("number?: {:?}", &code[..pos+1]);
+            *code = &code[pos..];
+            return 0
+        }
     }
     println!("value: {}", value);
     println!("index: {}", index);
