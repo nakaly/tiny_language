04a50099bbff2c592e023f115b6e49ae2460b4a3
nakaly <nakaly@linecorp.com>
2018-09-26 09:10:33 +0900

    Parse Polish notation (only plus)

--- src/main.rs
+++ src/main.rs
@@ -21,27 +21,54 @@ fn parse(input: &str) -> u32 {
 
 fn eval(code:  & mut &str) -> u32 {
     println!("{:?}", &code[0..1]);
-    let chars = code.chars();
+    skip_whitespace(code);
+    let mut chars = code.chars();
     let mut pos: usize = 0;
     let mut num: u32 = 0;
-    for (i, c) in chars.enumerate() {
-        if let Some(digit) = c.to_digit(10) {
-            pos = i;
-            num = digit + num * 10;
-            println!("p:{}", i);
-            println!("n:{}", num);
-            
+    let first_char = chars.next().unwrap();
+    if first_char.is_digit(10) {
+        num = first_char.to_digit(10).unwrap();
+        pos += 1;
+        for (i, c) in chars.enumerate() {
+            if let Some(digit) = c.to_digit(10) {
+                pos = i;
+                num = digit + num * 10;
+                println!("p:{}", i);
+                println!("n:{}", num);
+            } else {
+                pos = i + 1;
+                *code = &code[pos..];
+                return num;
+            }
         }
-        if c.is_whitespace() {
-            println!("number?: {:?}", &code[..pos+1]);
-            *code = &code[pos..];
-            return 0
+        *code = &code[pos..];
+        return num
+    } else {
+        match first_char {
+            '+' => {
+                *code = &code[pos + 1..];
+                num = eval(code) + eval(code);
+                num
+                },
+            _ => panic!("illegal character"),
         }
     }
-    println!("number?: {:?}", &code[..pos+1]);
-    println!("number: {:?}", num);
+
+}
+
+fn skip_whitespace(code: &mut &str) {
+    let chars = code.chars();
+    let mut pos: usize = 0;
+    for (i, c) in chars.enumerate() {
+        if c.is_whitespace() {
+            pos = i;
+        } else {
+            pos = i;
+            *code = &code[pos..];
+            return
+        }
+    }
     *code = &code[pos..];
-    num
 }
 
 #[cfg(test)]
@@ -50,5 +77,7 @@ mod tests {
     fn test() {
         use parse;
         assert_eq!(parse("1234"), 1234);
+        assert_eq!(parse("+ 1 2"), 3);
+        assert_eq!(parse("+ + 1 2 3"), 6);
     }
 }
